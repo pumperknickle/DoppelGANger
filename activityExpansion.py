fc37eafa4377a10e326a5c14be151463666e13b4
pumperknickle <pumperknickle@protonmail.com>
2020-08-22 10:59:42 -0500

    transformed data

--- activityExpansion.py
+++ activityExpansion.py
@@ -16,17 +16,17 @@ with open("tokenToSig.pkl", mode='rb') as tokenFile:
 
 print(tokensToSignatures)
 print(tokensToSignatures[5])
+#
+# real_seqs = extractSequences("real_samples.txt")
+#
+# real_sequences = []
+# for real_seq in real_seqs:
+#     real_sequence = []
+#     for idx in real_seq:
+#         real_sequence.append(tokensToSignatures[int(idx)])
+#     real_sequences.append(real_sequence)
 
-real_seqs = extractSequences("real_samples.txt")
-
-real_sequences = []
-for real_seq in real_seqs:
-    real_sequence = []
-    for idx in real_seq:
-        real_sequence.append(tokensToSignatures[int(idx)])
-    real_sequences.append(real_sequence)
-
-fake_seqs = extractSequences("generator_sample.txt")
+fake_seqs = extractSequences("genereted-packets-2.txt")
 
 fake_sequences = []
 for fake_seq in fake_seqs:
@@ -35,19 +35,19 @@ for fake_seq in fake_seqs:
         fake_sequence.append(tokensToSignatures[int(idx)])
     fake_sequences.append(fake_sequence)
 
-fake_sequences = random.sample(fake_sequences, len(real_sequences))
+# fake_sequences = random.sample(fake_sequences, len(real_sequences))
 
-final_reals = sequences_sample(real_sequences)
+# final_reals = sequences_sample(real_sequences)
 final_fakes = sequences_sample(fake_sequences)
 
-for i in range(len(final_reals)):
-  filename = 'exanded_real_samples.txt'
-  with open(filename, mode='a') as csvfile:
-    csv_writer = csv.writer(csvfile, delimiter=' ')
-    csv_writer.writerow(final_reals[i])
+# for i in range(len(final_reals)):
+#   filename = 'exanded_real_samples.txt'
+#   with open(filename, mode='a') as csvfile:
+#     csv_writer = csv.writer(csvfile, delimiter=' ')
+#     csv_writer.writerow(final_reals[i])
 
 for i in range(len(final_fakes)):
-  filename = 'exanded_generated_samples.txt'
+  filename = 'final_generated_packet_sizes.txt'
   with open(filename, mode='a') as csvfile:
     csv_writer = csv.writer(csvfile, delimiter=' ')
     csv_writer.writerow(final_fakes[i])
